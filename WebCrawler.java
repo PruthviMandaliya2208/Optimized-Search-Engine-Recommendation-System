4ed9eb9fc356d088af1c7dd16f5c707da76bce17
PruthviMandaliya2208 <pruthvimandaliya2208@gmail.com>
2017-02-07 00:39:44 +0530

    WebCrawler updated
    WebCrawler.java with all the required output and calculations done

--- WebCrawler.java
+++ WebCrawler.java
@@ -8,8 +8,10 @@ import java.io.IOException;
 
 public class WebCrawler {
 public String[] URL(String BaseFile) throws IOException{
-	File input = new File(BaseFile);
-	Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+	//File input = new File(BaseFile);
+
+	Document doc = Jsoup.connect(BaseFile).get();
+	//Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
 	
 	Elements links = doc.select("a[href]");
 	String textURL[] = new String[links.size()];
@@ -19,26 +21,61 @@ public String[] URL(String BaseFile) throws IOException{
 	}
 return textURL;	
 }
+private static void countStringOccurences(String[] strArray) {
+        HashMap<String, Integer> countMap = new HashMap<String, Integer>();
+        for (String string : strArray) {
+            if (!countMap.containsKey(string)) {
+                countMap.put(string, 1);
+            } else {
+                Integer count = countMap.get(string);
+                count = count + 1;
+                countMap.put(string, count);
+            }
+        }
+        printCount(countMap);
+    }
+private static void printCount(HashMap<String, Integer> countMap) {
+        Set<String> keySet = countMap.keySet();
+        for (String string : keySet) {
+            System.out.println(string + " : " + countMap.get(string));
+        }
+    }
 public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter URL from 'http': ");
+	String URLInput = sc.nextLine();
 	WebCrawler obj1 = new WebCrawler();
 	try{	
 		// First iteration finding out total links of basePage	
-		String[] URLS = obj1.URL("/home/dbit/Desktop/Pruthvi/Sample1.html"); //This is path which i have used for my configuration change this before using it.
-		int count = 0;
-		for(String opurl:URLS){
-			System.out.println("Base Page links "+ count++ +":"+opurl);
-		}
+		String[] URLS = obj1.URL(URLInput); //This is path which i have used for my configuration change this before using it.
+		int count = 0,rank=0;
 		int URLSsize = URLS.length;	
+		
+		countStringOccurences(URLS);
+
+		/*for(String opurl:URLS){
+			System.out.println("Base Page links "+ count++ +":"+opurl+" rank: "+rank);
+		}*/
+		
 		for(int i=1;i<URLSsize;i++){	
 			// First iteration finding out total links of basePage	
-			String[] URLS1 = obj1.URL(URLS[i]);
-			int count1 = 0;
-			for(String opurl1:URLS1){
-				System.out.println("Page : "+i+ " link "+ count1++ +":"+opurl1);
-			}	
+			if(URLS[i].startsWith("http://") || URLS[i].startsWith("www") || URLS[i].startsWith("/")){
+				String[] URLS1 = obj1.URL(URLS[i]);
+				/*int count1 = 0,URLCount = 0;
+				for(String opurl1:URLS1){
+				System.out.println("Page : "+i+ " link "+ count1++ +":"+opurl1+"Count: "+URLCount++);
+				}*/	
+				countStringOccurences(URLS1);
+			}
 		}
 	}catch(Exception e){
-		System.out.println(e);
+		if(e.toString()=="IllegalArgumentException"){
+			System.out.println("System cannot voilate security conditions");
+		}else{
+			System.out.println(e);
+		}
 	}
 }
 }
+
