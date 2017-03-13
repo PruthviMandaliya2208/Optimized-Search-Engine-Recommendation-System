c72a2f6f2e8ee0ad8af8999ae2eafa7507114490
Swapnil Mohite <swapnilmohite34@gmail.com>
2017-03-13 20:17:44 +0530

    TF Transformation in BM25

--- MyReducer2.java
+++ MyReducer2.java
@@ -9,11 +9,12 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class MyReducer2 extends Reducer<Text, DoubleWritable, Text, DoubleWritable>{
 	public void reduce(Text word, Iterable<DoubleWritable> counts, Context context) throws IOException, InterruptedException{	
 		DoubleWritable one = new DoubleWritable();
-		double sum=0;
+		double sum=0,T,k=1.2;
 		for(DoubleWritable count : counts){
 			sum += count.get();
 		}
-		one.set(sum);
+		T=(((k+1)*sum)/(sum+k));
+		one.set(T);
 		context.write(word,one);
 	}
 
